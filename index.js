ce9f88364bd76cb68a501b719b2c621cd3e8db8c
DanielPscolovki <151840169+DanielPscolovki@users.noreply.github.com>
2024-05-02 17:54:27 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1. Deklaruokite du kintamuosius petName ir petAge.
+let petName;
+let petAge;
+
+// 2. Inicializuokite deklaruotus kintamuosius.
+petName = "Barky";
+petAge = 5;
+
+// 3. Su console.log() atspausdinkite inicializuotus kintamuosius.
+console.log("Pet's name:", petName);
+console.log("Pet's age:", petAge);
+
+// 4. Pakeiskite kintamųjų litaralus.
+petName = "Whiskers";
+petAge = 3;
+
+// 5. Sukurkite kintamąjį pavadinimu buildingAddress ir jį inicializuokite. Kintamasis negali būti keičiamas.
+const buildingAddress = "123 Main Street";
+
+// 6. Su console.log() atspausdinkite inicializuotą kintamąjį.
+console.log("Building address:", buildingAddress);
